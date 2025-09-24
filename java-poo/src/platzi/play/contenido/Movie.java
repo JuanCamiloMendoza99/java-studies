851b2a1218caf8a6b68b9133683f7ff9dbef8496
Juan Camilo Mendoza <juan.camilo.mendoza99@gmail.com>
2025-09-24 18:55:41 -0500

    Add Movie class with properties and methods for playback and rating

---
+++ java-poo/src/platzi/play/contenido/Movie.java
@@ -0,0 +1,35 @@
+package platzi.play.contenido;
+
+public class Movie {
+    public String title;
+    public String description;
+    public int duration;
+    public String genre;
+    public int yearRelease;
+    public double rating;
+    public boolean isAvailable;
+
+    public void play() {
+        System.out.println("Playing movie: " + title);
+    }
+
+    public String getInformation() {
+        return "Title: " + title + "\n" +
+                "Description: " + description + "\n" +
+                "Duration: " + duration + " minutes\n" +
+                "Genre: " + genre + "\n" +
+                "Year of Release: " + yearRelease + "\n" +
+                "Rating: " + rating + "\n" +
+                "Available: " + (isAvailable ? "Yes" : "No");
+    }
+
+    public void rate(double newRating) {
+        if (newRating >= 0 && newRating <= 5) {
+            this.rating = newRating;
+            System.out.println("New rating for " + title + ": " + rating);
+        } else {
+            System.out.println("Rating must be between 0 and 5.");
+        }
+    }
+
+}
